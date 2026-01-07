070bbeb60f6784e1da7aa4ce8189bef945e84894
snehilshah <snehilshah27@gmail.com>
2026-01-07 12:18:05 +0530

    support treesitter main branch

--- lua/no-go/queries.lua
+++ lua/no-go/queries.lua
@@ -1,17 +1,18 @@
 local M = {}
 
 -- Only match if statements WITHOUT an else/else-if clause
--- The #not-has-parent prevents matching nested if statements in else-if chains
 -- The !alternative ensures we don't match if statements that have else clauses
+-- Note: As of tree-sitter-go main branch, block contains statement_list again
 M.error_query = [[
 (
   (if_statement
     condition: (binary_expression
       left: (identifier) @err_identifier)
     consequence: (block
-      (return_statement
-        (expression_list
-          (identifier) @return_identifier)?)) @collapse_block
+      (statement_list
+        (return_statement
+          (expression_list
+            (identifier) @return_identifier)?))) @collapse_block
     !alternative) @if_statement
 )
 ]]
